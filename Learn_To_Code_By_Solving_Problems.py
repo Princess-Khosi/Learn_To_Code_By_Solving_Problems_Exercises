1970867a0a7e6d9b755b05390ce50094b0473fbd
Princess Hlangu <your-github-princess27maia@gmail.com>
2025-12-08 17:17:24 +0200

    Song playlist problem solution

--- Learn_To_Code_By_Solving_Problems.py
+++ Learn_To_Code_By_Solving_Problems.py
@@ -156,43 +156,65 @@
 # SlotMachines()
 
 
-def SlotMachinesCount():
+# def SlotMachinesCount():
 
-    quarters = int(input())
-    first = int(input())
-    second = int(input())
-    third = int(input())
+#     quarters = int(input())
+#     first = int(input())
+#     second = int(input())
+#     third = int(input())
 
-    plays = 0
+#     plays = 0
 
-    while quarters >=1:
-        machine = plays % 3
-        quarters = quarters -1
+#     while quarters >=1:
+#         machine = plays % 3
+#         quarters = quarters -1
         
-        if machine == 0:
-            first = first +1
-            if first % 35 == 0:
-                quarters = quarters + 30
+#         if machine == 0:
+#             first = first +1
+#             if first % 35 == 0:
+#                 quarters = quarters + 30
         
-        elif machine == 1:
-            second = second + 1
-            if second % 100 == 0:
-                quarters = quarters + 60
+#         elif machine == 1:
+#             second = second + 1
+#             if second % 100 == 0:
+#                 quarters = quarters + 60
         
-        elif machine == 2:
-            third = third + 1
-            if third % 10 == 0:
-                quarters = quarters + 9
+#         elif machine == 2:
+#             third = third + 1
+#             if third % 10 == 0:
+#                 quarters = quarters + 9
 
-        plays = plays + 1
+#         plays = plays + 1
 
-    print(f"Martha plays, {plays} times before going broke.")
+#     print(f"Martha plays, {plays} times before going broke.")
 
-SlotMachinesCount()
+# SlotMachinesCount()
 
 
 def SongPlaylist()
 
+    songs = 'ABCDE'
+    button = 0
+
+    while button !=4:
+        button = int(input())
+        presses = int(input())
+
+        for i in range(presses):
+            if button == 1:
+                songs = song[1:] + songs[0]
+
+            elif button == 2:
+                songs = songs[-1] + songs[:-1]
+            
+            elif button == 3:
+                songs = songs[1] + songs[0] + songs[2:]
+
+    output = ""
+
+    for song in songs:
+        output = output + song + " "
     
+    print(output[:-1])
 
 SongPlaylist()
