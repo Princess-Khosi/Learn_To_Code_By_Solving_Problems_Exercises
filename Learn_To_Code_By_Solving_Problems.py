f8cc456c0be1acf519dff4dfc3d6e20245073af6
Princess Hlangu <your-github-princess27maia@gmail.com>
2025-12-09 23:35:47 +0200

    While loop in the case when we don't know the step size if we were to use a range for loop

--- Learn_To_Code_By_Solving_Problems.py
+++ Learn_To_Code_By_Solving_Problems.py
@@ -191,30 +191,49 @@
 # SlotMachinesCount()
 
 
-def SongPlaylist()
+# def SongPlaylist():
 
-    songs = 'ABCDE'
-    button = 0
+#     songs = 'ABCDE'
+#     button = 0
 
-    while button !=4:
-        button = int(input())
-        presses = int(input())
+#     while button !=4:
+#         button = int(input())
+#         presses = int(input())
 
-        for i in range(presses):
-            if button == 1:
-                songs = song[1:] + songs[0]
+#         for i in range(presses):
+#             if button == 1:
+#                 songs = song[1:] + songs[0]
 
-            elif button == 2:
-                songs = songs[-1] + songs[:-1]
+#             elif button == 2:
+#                 songs = songs[-1] + songs[:-1]
             
-            elif button == 3:
-                songs = songs[1] + songs[0] + songs[2:]
+#             elif button == 3:
+#                 songs = songs[1] + songs[0] + songs[2:]
 
-    output = ""
+#     output = ""
 
-    for song in songs:
-        output = output + song + " "
+#     for song in songs:
+#         output = output + song + " "
     
-    print(output[:-1])
+#     print(output[:-1])
 
-SongPlaylist()
+# SongPlaylist()
+
+
+def SecretSentence():
+
+    sentence = input()
+
+    result = ""
+    i = 0
+
+    while i < len(sentence):
+        result = result + sentence[i]
+        if sentence[i] in "aeiou":
+            i = i + 3
+        else:
+            i = i + 1
+    print(result)
+
+
+SecretSentence()
